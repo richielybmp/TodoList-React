e6bec2b50eadfcf4ab9f7a1291e76e2034a06976
Richiely Batista <richielybmp@hotmail.com>
2019-01-30 21:55:57 -0200

    Updates

---
+++ precache-manifest.bef5e4daaf8a27325ec84acca814b54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "0c35e9c40295a68f09bf",
+    "url": "/todo-list/static/js/main.0c35e9c4.chunk.js"
+  },
+  {
+    "revision": "a3b8bb696190695dc154",
+    "url": "/todo-list/static/js/1.a3b8bb69.chunk.js"
+  },
+  {
+    "revision": "0c35e9c40295a68f09bf",
+    "url": "/todo-list/static/css/main.29ad483a.chunk.css"
+  },
+  {
+    "revision": "a3b8bb696190695dc154",
+    "url": "/todo-list/static/css/1.bb65a26b.chunk.css"
+  },
+  {
+    "revision": "1c84d5cc37b7bb516c29d6867196836a",
+    "url": "/todo-list/index.html"
+  }
+];
